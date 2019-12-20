2bfbb63824446558c6cf390867ebb2c3be38e9aa
mwangiKibui <kenmwangi250@gmail.com>
2019-12-20 12:11:26 +0300

    Updates

---
+++ precache-manifest.84667458a111b03d179d32786b7f3e12.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b700414808c54e56dda4ce8883c4abaf",
+    "url": "/allcomrades/index.html"
+  },
+  {
+    "revision": "618b99d30ebd7d0c6cfa",
+    "url": "/allcomrades/static/css/2.2dab4a56.chunk.css"
+  },
+  {
+    "revision": "dbade82dc29691b2b30a",
+    "url": "/allcomrades/static/css/3.2718900d.chunk.css"
+  },
+  {
+    "revision": "9cc1e71eb5ab6161f28f",
+    "url": "/allcomrades/static/css/main.bbb5cb0b.chunk.css"
+  },
+  {
+    "revision": "618b99d30ebd7d0c6cfa",
+    "url": "/allcomrades/static/js/2.c8f83395.chunk.js"
+  },
+  {
+    "revision": "dbade82dc29691b2b30a",
+    "url": "/allcomrades/static/js/3.0e333348.chunk.js"
+  },
+  {
+    "revision": "41a559d83817df1385aa",
+    "url": "/allcomrades/static/js/4.f2ca6060.chunk.js"
+  },
+  {
+    "revision": "9cc1e71eb5ab6161f28f",
+    "url": "/allcomrades/static/js/main.0df70155.chunk.js"
+  },
+  {
+    "revision": "d826e1b54246137fb00c",
+    "url": "/allcomrades/static/js/runtime~main.6fd942a5.js"
+  },
+  {
+    "revision": "64456a81d57ffe49ab7dd0b37afff307",
+    "url": "/allcomrades/static/media/arrow-rounded-down-12x7.64456a81.svg"
+  },
+  {
+    "revision": "437746a0c8dfc78d2668150bda2e8f06",
+    "url": "/allcomrades/static/media/arrow-rounded-down-7x5.437746a0.svg"
+  },
+  {
+    "revision": "6c48d4a43ea34fab4743fb245f52068f",
+    "url": "/allcomrades/static/media/arrow-rounded-down-9x6.6c48d4a4.svg"
+  },
+  {
+    "revision": "08e1221c8fd5dccc3fd64b3073f4e418",
+    "url": "/allcomrades/static/media/arrow-rounded-left-6x9.08e1221c.svg"
+  },
+  {
+    "revision": "8099ab29c5c2bba2974383e607e8db21",
+    "url": "/allcomrades/static/media/arrow-rounded-left-7x11.8099ab29.svg"
+  },
+  {
+    "revision": "369ea40be3d51cf74e21e5208126dd9b",
+    "url": "/allcomrades/static/media/arrow-rounded-left-8x13.369ea40b.svg"
+  },
+  {
+    "revision": "6f4259951c5d6f78b9e6d31d7619b7a5",
+    "url": "/allcomrades/static/media/arrow-rounded-right-6x9.6f425995.svg"
+  },
+  {
+    "revision": "3b09c07989377c26cec9ee20cceb4adf",
+    "url": "/allcomrades/static/media/arrow-rounded-right-7x11.3b09c079.svg"
+  },
+  {
+    "revision": "0c286d3f2e268cd9f44f974f8b1fa48e",
+    "url": "/allcomrades/static/media/arrow-rounded-right-8x13.0c286d3f.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/allcomrades/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/allcomrades/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/allcomrades/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/allcomrades/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/allcomrades/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "567fd8b6504998ae8330b93f8ecc91e5",
+    "url": "/allcomrades/static/media/cart-20.567fd8b6.svg"
+  },
+  {
+    "revision": "804392d0162e80ff3ef6fe469cf936fb",
+    "url": "/allcomrades/static/media/check-12x9.804392d0.svg"
+  },
+  {
+    "revision": "671481aa81b8c32af2988a449f38de02",
+    "url": "/allcomrades/static/media/check-9x7.671481aa.svg"
+  },
+  {
+    "revision": "9f7817504c7d8d6a5e0c0b668b451c47",
+    "url": "/allcomrades/static/media/compare-16.9f781750.svg"
+  },
+  {
+    "revision": "528734c62d83f80b8b14d5ff6688c8da",
+    "url": "/allcomrades/static/media/cross-10.528734c6.svg"
+  },
+  {
+    "revision": "93070ac9dbb0132370872b23946cfe1c",
+    "url": "/allcomrades/static/media/cross-12.93070ac9.svg"
+  },
+  {
+    "revision": "75d4e9e04c88cc992600181aed936c64",
+    "url": "/allcomrades/static/media/cross-20.75d4e9e0.svg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/allcomrades/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "41f4f523d74c4b114dd985d4ec102a0f",
+    "url": "/allcomrades/static/media/event_advert.41f4f523.jpg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/allcomrades/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/allcomrades/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/allcomrades/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/allcomrades/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/allcomrades/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/allcomrades/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/allcomrades/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/allcomrades/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/allcomrades/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/allcomrades/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/allcomrades/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/allcomrades/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/allcomrades/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/allcomrades/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/allcomrades/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "56dc1a0ea477bc8df89910c56408ebf7",
+    "url": "/allcomrades/static/media/fi-24-hours-48.56dc1a0e.svg"
+  },
+  {
+    "revision": "fbb019adc21705d993c91316b3c08f0d",
+    "url": "/allcomrades/static/media/fi-free-delivery-48.fbb019ad.svg"
+  },
+  {
+    "revision": "8bbb999dc7f5eb6f649214d4c8ff06f6",
+    "url": "/allcomrades/static/media/fi-payment-security-48.8bbb999d.svg"
+  },
+  {
+    "revision": "00a295d7cd47d47febbb34f1a5a420f3",
+    "url": "/allcomrades/static/media/fi-tag-48.00a295d7.svg"
+  },
+  {
+    "revision": "fae20c059c759a5196e550978c37a49c",
+    "url": "/allcomrades/static/media/filters-16.fae20c05.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/allcomrades/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "93b6c6ffffb054c309ebab9bf2adaa59",
+    "url": "/allcomrades/static/media/heart-20.93b6c6ff.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/allcomrades/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/allcomrades/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/allcomrades/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/allcomrades/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/allcomrades/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0d029ab9735b68210e70a19df2388df9",
+    "url": "/allcomrades/static/media/karu_1.0d029ab9.jpg"
+  },
+  {
+    "revision": "afd15d80f6990d3ec7c182d5a91664b5",
+    "url": "/allcomrades/static/media/karu_2.afd15d80.jpg"
+  },
+  {
+    "revision": "561a6f5a1e1353ea0ab70d3275b9adeb",
+    "url": "/allcomrades/static/media/layout-grid-16x16.561a6f5a.svg"
+  },
+  {
+    "revision": "a13d95e4a3dd3c0c7f91f0f4f21a92f4",
+    "url": "/allcomrades/static/media/layout-grid-with-details-16x16.a13d95e4.svg"
+  },
+  {
+    "revision": "e1a94a2e0ee4353edcefa42b7147075e",
+    "url": "/allcomrades/static/media/layout-list-16x16.e1a94a2e.svg"
+  },
+  {
+    "revision": "e16633dfbd8c82c62765d3c5cbc94b53",
+    "url": "/allcomrades/static/media/logo-small.e16633df.svg"
+  },
+  {
+    "revision": "833bcde2d9ae93ba75c47819c1bffd84",
+    "url": "/allcomrades/static/media/logo.833bcde2.svg"
+  },
+  {
+    "revision": "e45adc284be204a0283349642b176f8a",
+    "url": "/allcomrades/static/media/logo.e45adc28.png"
+  },
+  {
+    "revision": "9414eccc2e7b070d14a13bbb348b900a",
+    "url": "/allcomrades/static/media/menu-18x14.9414eccc.svg"
+  },
+  {
+    "revision": "fe4b4e4984bee1ea2b93301466af0457",
+    "url": "/allcomrades/static/media/mp_advert.fe4b4e49.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/allcomrades/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/allcomrades/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/allcomrades/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/allcomrades/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/allcomrades/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a0532c00ace441528fb2d019be460239",
+    "url": "/allcomrades/static/media/quickview-16.a0532c00.svg"
+  },
+  {
+    "revision": "1b14329a18318cf2ad9a2da07b6face0",
+    "url": "/allcomrades/static/media/rating-13x12.1b14329a.svg"
+  },
+  {
+    "revision": "5d954e6d4436b7f9bf2e177a82133d36",
+    "url": "/allcomrades/static/media/search-20.5d954e6d.svg"
+  },
+  {
+    "revision": "fa274ec1ce1c6539b07bbd780f232d88",
+    "url": "/allcomrades/static/media/wishlist-16.fa274ec1.svg"
+  }
+]);
